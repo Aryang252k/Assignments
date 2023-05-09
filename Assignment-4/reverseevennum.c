1cdc94911e4e934e025ed7d7e1662ec60a4a635e
Aryan gupta <116788125+Aryang252k@users.noreply.github.com>
2023-05-09 05:36:44 +0000

    assignment 4 compleled

---
+++ Assignment-4/reverseevennum.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+void main(){
+int i=20;
+while(i>=1) {
+ if(i%2==0)
+ printf("%d\n",i);
+ i--;
+}
+}
---
+++ Assignment-4/table5.c
@@ -0,0 +1,8 @@
+# include<stdio.h>
+void main(){
+int i=1;
+while(i<=10) {
+ printf("5 * %d = %d\n",i,i*5);
+i++;
+}
+}
