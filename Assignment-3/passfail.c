0ad24c846a9ba7cb267a91d7e9183b1b918caf04
Aryan gupta <116788125+Aryang252k@users.noreply.github.com>
2023-05-10 19:33:12 +0000

    assignment 3 passfail solved

--- Assignment-3/passfail.c
+++ Assignment-3/passfail.c
@@ -4,23 +4,20 @@ void main(){
    printf("Enter marks of five subject: ");
    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
    s=a+b+c+d+e;
-
-
-    if(a>=0 || b>=0 || c>=0 || d>=0 || e>=0 && a<=100 || b<=100 || c<=100 || d<=100 || e<=100)
+    if((a>=0) && (b>=0 ) && (c>=0) && (d>=0) && (e>=0) && (a<=100) && (b<=100) && (c<=100) && (d<=100) && (e<=100))
    {
-     printf("INVALIDE MARKS!\n");
-   }
-   else if( a<33 || b<33 || c<33 || d<33 || e<33)
+     if( a<33 || b<33 || c<33 || d<33 || e<33)
    {
-     printf("FAILED as you are not passed each subject!\n");
+     printf("FAILED as you are not passed in each subject!\n");
    }
    else if(s>=165)
    {
     printf("You scored %d and PASSED.\n",s);
    }
-   else
-   {
-      printf("You scored %d and FAILED.\n",s);
    }
+  else
+   {  
+    printf("INVALIDE MARKS!\n");
+  }
 
 }
